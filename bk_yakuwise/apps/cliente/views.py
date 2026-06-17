b007815d264bf869ad40be37ccad989a7aa64691
Yessenia Huaman <yesel1601@hotmail.com>
2026-06-17 15:55:45 -0500

    [ADD] Conexión DRF - Angular (#4)

--- bk_yakuwise/apps/cliente/views.py
+++ bk_yakuwise/apps/cliente/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+# apps/cliente/views.py
+from rest_framework import viewsets
+
+from .models import Cliente
+from .serializers import ClienteSerializer
+
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    print('si llego a views :)')
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer

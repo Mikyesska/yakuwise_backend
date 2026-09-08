347f98621d6df889c36110f4dfcbab543859754c
Yessenia Eliana <yesel1601@hotmail.com>
2026-09-08 18:56:49 -0500

    [ADD] Endpoints para modelos independientes de monitoreoparametro

--- bk_yakuwise/apps/monitoreoparametro/views.py
+++ bk_yakuwise/apps/monitoreoparametro/views.py
@@ -1,1 +1,48 @@
-# Create your views here.
+from rest_framework import viewsets
+
+from .models import (
+    FrecuenciaMonitoreo,
+    HorarioMonitoreo,
+    TipoError,
+    TipoParametro,
+    TipoPlanta,
+    UnidadMedida,
+)
+from .serializers import (
+    FrecuenciaMonitoreoSerializer,
+    HorarioMonitoreoSerializer,
+    TipoErrorSerializer,
+    TipoParametroSerializer,
+    TipoPlantaSerializer,
+    UnidadMedidaSerializer,
+)
+
+
+class FrecuenciaMonitoreoViewSet(viewsets.ModelViewSet):
+    queryset = FrecuenciaMonitoreo.objects.all()
+    serializer_class = FrecuenciaMonitoreoSerializer
+
+
+class HorarioMonitoreoViewSet(viewsets.ModelViewSet):
+    queryset = HorarioMonitoreo.objects.all()
+    serializer_class = HorarioMonitoreoSerializer
+
+
+class TipoParametroViewSet(viewsets.ModelViewSet):
+    queryset = TipoParametro.objects.all()
+    serializer_class = TipoParametroSerializer
+
+
+class UnidadMedidaViewSet(viewsets.ModelViewSet):
+    queryset = UnidadMedida.objects.all()
+    serializer_class = UnidadMedidaSerializer
+
+
+class TipoErrorViewSet(viewsets.ModelViewSet):
+    queryset = TipoError.objects.all()
+    serializer_class = TipoErrorSerializer
+
+
+class TipoPlantaViewSet(viewsets.ModelViewSet):
+    queryset = TipoPlanta.objects.all()
+    serializer_class = TipoPlantaSerializer
--- bk_yakuwise/yakuwise/urls.py
+++ bk_yakuwise/yakuwise/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.cliente.urls')),  # ejemplo
     path('security/', include('apps.security.urls')),
+    path('api/monitoreo/', include('apps.monitoreoparametro.urls')),
 ]
